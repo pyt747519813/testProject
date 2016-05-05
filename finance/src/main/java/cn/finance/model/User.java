cbc908dbdf4085c2fe9d067df49fd1659209e4a1
pyt <yongting.pan@rextec.com.cn>
2016-05-05 18:10:43 +0800

    test

---
+++ finance/src/main/java/cn/finance/model/User.java
@@ -0,0 +1,28 @@
+package cn.finance.model;
+
+/**
+ * Created by REXTEC-15-2 on 2016-05-05.
+ */
+public class User {
+    private int id;
+    private int state;
+    private String nickname;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getState() {
+        return state;
+    }
+    public void setState(int state) {
+        this.state = state;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
